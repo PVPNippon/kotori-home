840393de6a0147b9b2a65953f0d1986b6612e186
Joshi <c.joshi@pvp.co.jp>
2024-10-25 15:31:49 +0900

    privacy and tnc localization

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 //TODO: configure function so that it waits until all DOM elements are loaded to run
 
+function toggleDropdown(event, dropdownId) {
+  event.preventDefault(); // Prevents the page from jumping
 
+   // Hide all dropdowns first
+   document.querySelectorAll('ul[id^="privacyDropdown"], ul[id^="termsDropdown"]').forEach(dropdown => {
+    dropdown.classList.add('hidden');
+  });
+
+  // Toggle the selected dropdown visibility
+  const dropdown = document.getElementById(dropdownId);
+  dropdown.classList.toggle('hidden');
+}
 
 document.addEventListener("DOMContentLoaded", function() {
 
 const btn = document.querySelector('button.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
+
+
 
 // add event listeners
 btn.addEventListener("click", () => {
@@ -23,7 +36,7 @@ btn.addEventListener("click", () => {
     
   } else {
     // Check if the page is accessed via GitHub Pages
-    if (!window.location.href.includes("kotori-bounce.com")) {
+    if (!window.location.href.includes("kotori-bounce.com") && !window.location.href.includes("127.0.0.1")) {  
       // Hide the header if not on GitHub Pages
       document.getElementById('navbar').style.display = 'none';
       document.getElementById('footer').style.display = 'none';
